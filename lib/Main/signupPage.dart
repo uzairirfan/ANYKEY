1adea35bbab6e55c8f08e9488b6a8f20f300ce17
uzairirfan <m.uzair.i@hotmail.com>
2020-03-26 03:14:09 -0400

    added sign in page with validation

--- lib/Main/signupPage.dart
+++ lib/Main/signupPage.dart
@@ -27,68 +27,70 @@ class _RegisterPage extends State<RegisterPage> {
       body: Container(
         padding: EdgeInsets.all(20.0),
         child: Form(
+            key: _formKey,
             child: Column(
-          children: <Widget>[
-            SizedBox(height: 20.0),
-            Text(
-              'Registration Information',
-              style: TextStyle(fontSize: 20),
-            ),
-            SizedBox(height: 20.0),
-            TextFormField(
-                validator: (value) {
-                  if (value.isNotEmpty) {
-                    _email = value;
-                    assert(EmailValidator.validate(_email));
-                    return null;
-                  }
-                  return "Please Enter an Email.";
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(labelText: "Email Address")),
-            TextFormField(
-                validator: (value) {
-                  if (value.isNotEmpty) {
-                    _username = value;
-                    return null;
-                  }
-                  return "Please Enter a Username.";
-                },
-                decoration: InputDecoration(labelText: "Username")),
-            TextFormField(
-                validator: (value) {
-                  if (value.isNotEmpty) {
-                    _password = value;
-                    return null;
-                  }
-                  return "Please Enter a Password.";
-                },
-                obscureText: true,
-                decoration: InputDecoration(labelText: "Password")),
-            SizedBox(height: 20.0),
-            new CheckboxListTile(
-              title: Text("Are you an owner?"),
-              value: owner,
-              activeColor: Theme.of(context).primaryColor,
-              onChanged: (bool value) {
-                setState(() {
-                  owner = value;
-                });
-              },
-              controlAffinity: ListTileControlAffinity.leading,
-            ),
-            RaisedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, otherwise false.
-                if (_formKey.currentState.validate()) {
-                  if (owner) _type = "admin";
-                  saveUser(_email, _username, _password, _type);
-                }
-              },
-              child: Text('Sign Up!'),
-            ),
-          ],
-        )),
+              children: <Widget>[
+                SizedBox(height: 20.0),
+                Text(
+                  'Registration Information',
+                  style: TextStyle(fontSize: 20),
+                ),
+                SizedBox(height: 20.0),
+                TextFormField(
+                    validator: (value) {
+                      if (value.isNotEmpty) {
+                        _email = value;
+                        if(!EmailValidator.validate(_email))return "Please Enter an Email.";
+                        return null;
+                      }
+                      return "Please Enter an Email.";
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(labelText: "Email Address")),
+                TextFormField(
+                    validator: (value) {
+                      if (value.isNotEmpty) {
+                        _username = value;
+                        return null;
+                      }
+                      return "Please Enter a Username.";
+                    },
+                    decoration: InputDecoration(labelText: "Username")),
+                TextFormField(
+                    validator: (value) {
+                      if (value.isNotEmpty) {
+                        _password = value;
+                        return null;
+                      }
+                      return "Please Enter a Password.";
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(labelText: "Password")),
+                SizedBox(height: 20.0),
+                new CheckboxListTile(
+                  title: Text("Are you an owner?"),
+                  value: owner,
+                  activeColor: Theme.of(context).primaryColor,
+                  onChanged: (bool value) {
+                    setState(() {
+                      owner = value;
+                    });
+                  },
+                  controlAffinity: ListTileControlAffinity.leading,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, otherwise false.
+                    if (_formKey.currentState.validate()) {
+                      if (owner) _type = "admin";
+                      saveUser(_email, _username, _password, _type);
+                      Navigator.pop(context);
+                    }
+                  },
+                  child: Text('Sign Up!'),
+                ),
+              ],
+            )),
       ),
     );
   }
