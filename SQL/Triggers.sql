b951a8a0e98b639ab00a003f79b4a6fdec1f4843
uzairirfan <54855337+uzairirfan@users.noreply.github.com>
2020-04-06 18:16:12 -0400

    Update and rename trigger.txt to Triggers.sql

---
+++ SQL/Triggers.sql
@@ -0,0 +1,14 @@
+
+create trigger buy_from_publisher
+	after update
+	on game_ware
+	for each row
+	execute procedure auto_buy()
+	
+
+
+create trigger sell_stock_warhouse
+	after insert
+	on game_order
+	for each row
+	execute procedure sell_stock()
