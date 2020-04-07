d34bbc69b3739b98cf26b780de32ce54032e9790
uzairirfan <54855337+uzairirfan@users.noreply.github.com>
2020-04-07 17:41:13 -0400

    Update Triggers.sql

--- SQL/Triggers.sql
+++ SQL/Triggers.sql
@@ -1,10 +1,11 @@
+--Creates a trigger to auto buy a game every time there is an update on game_ware (such as quantity being removed)
 create trigger buy_from_publisher
 	after update
 	on game_ware
 	for each row
 	execute procedure auto_buy()
 	
-
+--Creates a trigger to sell stock after there is an order made
 create trigger sell_stock_warhouse
 	after insert
 	on game_order
